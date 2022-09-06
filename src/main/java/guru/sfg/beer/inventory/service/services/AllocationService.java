eb5ca210e42ac6d36c8363211c8fc3f45becb1bf
BASSET, Thomas <thomas.basset@cgi.com>
2022-09-06 11:56:16 +0200

    Consume Deallocation Request. closes #12

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -3,6 +3,7 @@ package guru.sfg.beer.inventory.service.services;
 import guru.sfg.brewery.model.BeerOrderDto;
 
 public interface AllocationService {
-
     Boolean allocateOrder(BeerOrderDto beerOrderDto);
+
+    void deallocateOrder(BeerOrderDto beerOrderDto);
 }
